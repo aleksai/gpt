237da6b71c844f197c20bd5158bf9af515d3237c
Aleksei Pugachev <lenyapugachev@me.com>
2023-02-08 12:59:04 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -4,22 +4,28 @@ import { message } from "telegraf/filters"
 import { createClient } from "redis"
 
 const onMessage = async (ctx) => {
-	ctx.sendChatAction("typing")
+	try {
+		ctx.sendChatAction("typing")
 
-	const conversationId = await redis.get("conversationId_" + ctx.message.chat.id)
-	const parentMessageId = await redis.get("parentMessageId_" + ctx.message.chat.id)
+		const conversationId = await redis.get("conversationId_" + ctx.message.chat.id)
+		const parentMessageId = await redis.get("parentMessageId_" + ctx.message.chat.id)
 
-	const res = await api.sendMessage(ctx.message.text, conversationId && parentMessageId ? { conversationId, parentMessageId } : {})
+		const res = await api.sendMessage(ctx.message.text, conversationId && parentMessageId ? { conversationId, parentMessageId } : {})
 
-	await redis.set("conversationId_" + ctx.message.chat.id, res.conversationId)
-	await redis.set("parentMessageId_" + ctx.message.chat.id, res.id)
+		await redis.set("conversationId_" + ctx.message.chat.id, res.conversationId)
+		await redis.set("parentMessageId_" + ctx.message.chat.id, res.id)
 
-	ctx.reply(res.text)
+		ctx.reply(res.text)
+	} catch(error) {
+		console.log("ERROR", error)
+
+		ctx.reply("Try again later")
+	}
 }
 
 const api = new ChatGPTAPI({ apiKey: process.env.OPENAI_API_KEY })
 const bot = new Telegraf(process.env.TELEGRAM_API_KEY)
-const redis = createClient({ url: "redis://" + process.env.REDIS })
+const redis = createClient({ url: "redis://" + process.env.REDIS, password: process.env.REDIS_PASSWORD })
 
 redis.on("error", error => console.log("Redis Client Error", error))
 
