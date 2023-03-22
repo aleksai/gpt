c0d962bbd31fa4d2013675cac46e25ef47068770
Aleksei Pugachev <lenyapugachev@me.com>
2023-03-23 00:28:33 +0300

    Production

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import { createClient } from "redis"
 
 import text from "./inputs/text.js"
 
-const api = new ChatGPTAPI({ apiKey: process.env.OPENAI_API_KEY, debug: true, completionParams: { model: "gpt-3.5-turbo-0301" }})
+const api = new ChatGPTAPI({ 
+	apiKey: process.env.OPENAI_API_KEY,
+	completionParams: { model: "gpt-3.5-turbo-0301" },
+	// debug: true
+})
 const bot = new Telegraf(process.env.TELEGRAM_API_KEY)
-const redis = createClient({ legacyMode: true, url: "redis://:" + process.env.REDIS_PASSWORD + "@" + process.env.REDIS + ":6379" })
+const redis = createClient({
+	// legacyMode: true,
+	url: "redis://:" + process.env.REDIS_PASSWORD + "@" + process.env.REDIS + ":6379"
+})
 
 redis.on("error", error => console.log("🛑 ERROR REDIS", error))
 await redis.connect()
